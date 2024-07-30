7128a437e50b3beff74a69cefbf6e75d35975301
Jason Stajich <jasonstajich.phd@gmail.com>
2024-07-29 21:30:38 -0700

    add beginning framework

---
+++ src/MATPredict/__init__.py
@@ -0,0 +1,26 @@
+"""Required actions when initiating the package."""
+
+try:
+    from importlib_metadata import entry_points, metadata
+except ImportError:
+    from importlib.metadata import entry_points, metadata
+
+import logging
+
+
+def _map_entry_point_module(project_name):
+    """Get the dictionary that map the scripts to their entry point names."""
+    d = {}
+    for x in entry_points().select(group="console_scripts"):
+        if x.dist.name == project_name:
+            d[x.module] = x.name
+    return d
+
+
+_meta = metadata("MATPredict")
+__version__ = _meta["Version"]
+__author__ = _meta["Author-email"]
+__entry_points__ = _map_entry_point_module(_meta["Name"])
+
+logging.basicConfig(format="%(asctime)s %(name)s %(levelname)s %(message)s", level="INFO")
+logger = logging.getLogger(__name__)
